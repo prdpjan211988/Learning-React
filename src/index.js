9531016983e15c11baa16ffa8a116b167bdab61a
Pradeep Kumar H D <Pradeepkumar.HD@Ltts.com>
2019-12-04 14:58:16 +0530

    second commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+
+    return <div> hi pradeep </div>
+};
+ReactDOM.render(<App />, document.querySelector('#root'))
